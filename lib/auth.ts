73a6ce8abc5e42e70ec91a892c2ee21f0470d5b0
sama-004 <70210929+Sama-004@users.noreply.github.com>
2024-07-20 13:14:45 +0530

    auth to db
    signin and singups to db done

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,34 +1,70 @@
 import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
 import { signIn } from "next-auth/react";
+import bcrypt from "bcrypt";
+import prisma from "../db";
 
 export const authOptions = {
   providers: [
     CredentialsProvider({
       name: "Credentials",
       credentials: {
-        username: {
+        email: {
           label: "email",
           type: "text",
-          placeholder: "email@gmail.com",
+          placeholder: "Enter you email",
         },
         password: {
           label: "password",
           type: "password",
-          placeholder: "********",
+          placeholder: "Enter your password",
         },
       },
       async authorize(credentials: any) {
-        return {
-          id: "user1",
-          name: "asd",
-          userId: "asd",
-          email: "ramdomEmail",
-        };
+        const hashedPassword = await bcrypt.hash(credentials.password, 10);
+        const existingUser = await prisma.user.findUnique({
+          where: {
+            email: credentials.email as string,
+          },
+        });
+        if (existingUser) {
+          const passwordValidation = await bcrypt.compare(
+            credentials.password,
+            existingUser.password
+          );
+          if (passwordValidation) {
+            return {
+              id: existingUser.id.toString(),
+              name: existingUser.name,
+              username: existingUser.username,
+              email: existingUser.email,
+            };
+          }
+          return null;
+        }
+        try {
+          const user = await prisma.user.create({
+            data: {
+              email: credentials.email as string,
+              password: hashedPassword,
+              username: "test",
+              name: "testing",
+            },
+          });
+          return {
+            id: user.id.toString(),
+            name: user.name,
+            email: user.email,
+            username: user.username,
+          };
+        } catch (err) {
+          console.error(err);
+        }
+        return null;
       },
     }),
   ],
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: process.env.NEXTAUTH_SECRET || "secret",
   callbacks: {
     jwt: async ({ user, token }: any) => {
       if (user) {
