5b9ae1f869a58f8779598d63ee78d465f4a5992c
sama-004 <70210929+Sama-004@users.noreply.github.com>
2024-07-23 20:25:07 +0530

    removed shadcn chat
    kuch smjh nhi aarha tha

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,26 +1,8 @@
-import { ChatLayout } from "@/components/chat/chat-layout";
-import { getServerSession } from "next-auth/next";
-import { authOptions } from "@/lib/auth";
-import Navbar from "@/components/nav";
-import { cookies } from "next/headers";
+export default function Home(){
 
-export default async function Home() {
-  const session = await getServerSession(authOptions);
-  const layout = cookies().get("react-resizable-panels:layout");
-  const defaultLayout = layout ? JSON.parse(layout.value) : undefined;
-  return (
-    <>
-      {session ? (
-        <div>
-          <Navbar />
-          {/* <div className="z-10 border rounded-lg max-w-5xl w-full h-full text-sm lg:flex"> */}
-          <div className="w-full h-full lg:flex rounded-lg z-10 border text-sm max-w-10xl">
-            <ChatLayout defaultLayout={defaultLayout} navCollapsedSize={8} />
-          </div>
-        </div>
-      ) : (
-        <div>Not logged in</div>
-      )}
-    </>
-  );
+return(
+  <div>
+    Write your message here 
+  </div>
+)
 }
